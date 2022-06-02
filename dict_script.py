2be8117783ca90d22a17f5398f733b903b70845b
EmmetH11 <57051119+EmmetH11@users.noreply.github.com>
2022-06-02 10:21:26 -0400

    Add files via upload

--- dict_script.py
+++ dict_script.py
@@ -3,6 +3,7 @@ import pipeline
 from config import *
 import numpy as np
 
+# Read feature vectorizer
 f = open(TFID_PATH, "rb")
 vectorizer = pickle.load(f)
 f.close()
@@ -11,11 +12,13 @@ f = open("english_dict.txt", "r")
 dict = f.read().splitlines()
 f.close()
 
+# Generate bigrams 
 inputs = {}
 for a in dict:
     for b in dict:
         inputs[a + "_" + b] = pipeline.transform_words(a, b, vectorizer)
 
+# Read model so that it does not have to be retrained
 f = open(SVC_PATH, "rb")
 model = pickle.load(f)
 f.close()
@@ -23,6 +26,7 @@ bigrams = {}
 for key, X in inputs.items():
     bigrams[key] = model.predict(X)
 
+# sort and write rankings to file
 f = open(RANKING_PATH, "w")
 sort_orders = sorted(bigrams.items(), key=lambda x: x[1], reverse=True)
 for i in sort_orders:
